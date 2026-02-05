dfc8d6e815aabe5c2e2316aeeda02fcbe276efee
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2026-02-05 16:48:35 +0100

    Créer la methode creerEmploye dans service

--- src/app/dashboards/admin/services/employe/employe.service.ts
+++ src/app/dashboards/admin/services/employe/employe.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Employe } from '../../models/employe/employe.model';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../../models/shared/api-response.model';
+import { CreateEmployeRequest } from '../../models/employe/create-employe-request.model';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,10 @@ export class EmployeService {
 
   getAllEmployes(): Observable<ApiResponse<Employe[]>> {
     return this.http.get<ApiResponse<Employe[]>>(this.baseUrl);
+  }
+
+  createEmploye(newEmploye: CreateEmployeRequest): Observable<ApiResponse<Employe>> {
+    return this.http.post<ApiResponse<Employe>>(this.baseUrl,newEmploye);
   }
   
 }
